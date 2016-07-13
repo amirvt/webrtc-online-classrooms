7df84bc02461cccb694c8d5fa0eec0d08f458318
Amir Vakili <amirvt92@gmail.com>
2016-07-13 15:18:49 +0430

    added chrome extension

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(express.static(__dirname + '/../dist'));
 
 //TODO make into constant
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Credentials", "true");
   next();
